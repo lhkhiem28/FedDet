a3aab29d51e53cd314681410f3f64808235a7feb
lhkhiem28 <lhkhiem28@gmail.com>
2022-11-18 03:43:45 +0000

    initial

--- source/client.py
+++ source/client.py
@@ -66,70 +66,71 @@ class Client(fl.client.NumPyClient):
             self.fitting_verbose, 
         )
         self.round += 1
-        return self.get_parameters(config = {}), len(fit_loaders["fit"].dataset), results
+        return self.get_parameters(config = {}), len(self.fit_loaders["fit"].dataset), results
     def evaluate(self, 
         parameters, config, 
     ):
-        return float(len(fit_loaders["evaluate"].dataset)), len(fit_loaders["evaluate"].dataset), {}
+        return float(len(self.fit_loaders["evaluate"].dataset)), len(self.fit_loaders["evaluate"].dataset), {}
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--server_address", type = str, default = "127.0.0.1"), parser.add_argument("--server_port", type = int)
-parser.add_argument("--dataset", type = str, default = "VOC2007"), parser.add_argument("--cid", type = int)
-parser.add_argument("--num_rounds", type = int, default = 250)
-parser.add_argument("--num_epochs", type = int, default = 5)
-args = parser.parse_args()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--server_address", type = str, default = "127.0.0.1"), parser.add_argument("--server_port", type = int)
+    parser.add_argument("--dataset", type = str, default = "VOC2007"), parser.add_argument("--cid", type = int)
+    parser.add_argument("--num_rounds", type = int, default = 250)
+    parser.add_argument("--num_epochs", type = int, default = 5)
+    args = parser.parse_args()
 
-datasets = {
-    "fit":DetImageDataset(
-        images_path = "../datasets/VOC2007/clients/client_{}/fit/images".format(args.cid), labels_path = "../datasets/VOC2007/clients/client_{}/fit/labels".format(args.cid)
-        , image_size = 416
-        , augment = True
-        , multiscale = True
-    ), 
-    "evaluate":DetImageDataset(
-        images_path = "../datasets/VOC2007/clients/client_{}/evaluate/images".format(args.cid), labels_path = "../datasets/VOC2007/clients/client_{}/evaluate/labels".format(args.cid)
-        , image_size = 416
-        , augment = False
-        , multiscale = False
-    ), 
-}
-fit_loaders = {
-    "fit":torch.utils.data.DataLoader(
-        datasets["fit"], collate_fn = datasets["fit"].collate_fn, 
-        num_workers = 2, batch_size = 4, 
-        shuffle = True, 
-    ), 
-    "evaluate":torch.utils.data.DataLoader(
-        datasets["evaluate"], collate_fn = datasets["evaluate"].collate_fn, 
-        num_workers = 2, batch_size = 4, 
-        shuffle = False, 
-    ), 
-}
-model = Darknet("pytorchyolo/configs/yolov3.cfg")
-model.load_darknet_weights("../ckps/darknet53.conv.74")
-optimizer = optim.Adam(
-    model.parameters(), 
-    lr = model.hyperparams["lr"], weight_decay = model.hyperparams["weight_decay"], 
-)
-lr_scheduler = optim.lr_scheduler.CosineAnnealingLR(
-    optimizer, 
-    eta_min = 0.01*model.hyperparams["lr"], T_max = int(0.92*args.num_rounds), 
-)
-save_ckp_dir = "../ckps/{}".format(args.dataset)
-if not os.path.exists(save_ckp_dir):
-    os.makedirs(save_ckp_dir)
-client = Client(
-    fit_loaders, 
-    model, 
-    args.num_epochs, 
-    args.num_rounds, 
-    optimizer, 
-    lr_scheduler, 
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu"), 
-    save_ckp_dir = save_ckp_dir, 
-    fitting_verbose = True, 
-)
-fl.client.start_numpy_client(
-    server_address = "{}:{}".format(args.server_address, args.server_port), 
-    client = client, 
-)
+    datasets = {
+        "fit":DetImageDataset(
+            images_path = "../datasets/VOC2007/clients/client_{}/fit/images".format(args.cid), labels_path = "../datasets/VOC2007/clients/client_{}/fit/labels".format(args.cid)
+            , image_size = 416
+            , augment = True
+            , multiscale = True
+        ), 
+        "evaluate":DetImageDataset(
+            images_path = "../datasets/VOC2007/clients/client_{}/evaluate/images".format(args.cid), labels_path = "../datasets/VOC2007/clients/client_{}/evaluate/labels".format(args.cid)
+            , image_size = 416
+            , augment = False
+            , multiscale = False
+        ), 
+    }
+    fit_loaders = {
+        "fit":torch.utils.data.DataLoader(
+            datasets["fit"], collate_fn = datasets["fit"].collate_fn, 
+            num_workers = 2, batch_size = 4, 
+            shuffle = True, 
+        ), 
+        "evaluate":torch.utils.data.DataLoader(
+            datasets["evaluate"], collate_fn = datasets["evaluate"].collate_fn, 
+            num_workers = 2, batch_size = 4, 
+            shuffle = False, 
+        ), 
+    }
+    model = Darknet("pytorchyolo/configs/yolov3.cfg")
+    model.load_darknet_weights("../ckps/darknet53.conv.74")
+    optimizer = optim.Adam(
+        model.parameters(), 
+        lr = model.hyperparams["lr"], weight_decay = model.hyperparams["weight_decay"], 
+    )
+    lr_scheduler = optim.lr_scheduler.CosineAnnealingLR(
+        optimizer, 
+        eta_min = 0.01*model.hyperparams["lr"], T_max = int(0.92*args.num_rounds), 
+    )
+    save_ckp_dir = "../ckps/{}".format(args.dataset)
+    if not os.path.exists(save_ckp_dir):
+        os.makedirs(save_ckp_dir)
+    client = Client(
+        fit_loaders, 
+        model, 
+        args.num_epochs, 
+        args.num_rounds, 
+        optimizer, 
+        lr_scheduler, 
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu"), 
+        save_ckp_dir = save_ckp_dir, 
+        fitting_verbose = True, 
+    )
+    fl.client.start_numpy_client(
+        server_address = "{}:{}".format(args.server_address, args.server_port), 
+        client = client, 
+    )
